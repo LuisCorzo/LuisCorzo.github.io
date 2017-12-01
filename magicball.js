68b9f82d2684ce0af7f57b4f9eee87df4208235b
LuisCorzo <31773443+LuisCorzo@users.noreply.github.com>
2017-12-01 09:33:54 -0800

    push dec/1/2017
    hello

--- magicball.js
+++ magicball.js
@@ -29,7 +29,42 @@ function shakeeightball() {
 
 /* below is code for tic tac toe*/
 
+
+
+// holds x or o's
+var character = "O";
+
+
+
+
+
 //handles x and o turns
 function turn(location){
-	document.getElementById(location).innerHTML= "x" ;
+    
+    
+       if(character == "X") {
+       	  character = "O";
+       	  }
+        else{
+        	character="X";
+        }
+
+	document.getElementById(location).innerHTML= character; 
+}
+
+
+
+
+// clears X's and O'x
+
+function clearAll(){
+document.getElementById("r1c1").innerHTML = "";
+document.getElementById("r1c2").innerHTML = "";
+document.getElementById("r1c3").innerHTML = "";
+document.getElementById("r2c1").innerHTML = "";
+document.getElementById("r2c2").innerHTML = "";
+document.getElementById("r2c3").innerHTML = "";
+document.getElementById("r3c1").innerHTML = "";
+document.getElementById("r3c2").innerHTML = "";
+document.getElementById("r3c3").innerHTML = "";
 }
