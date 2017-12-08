da280fd3df464492a974fb6cea78e4c6afdbd9cc
LuisCorzo <31773443+LuisCorzo@users.noreply.github.com>
2017-12-08 09:30:16 -0800

    dec/8/2017
    :)

--- magicball.js
+++ magicball.js
@@ -40,7 +40,7 @@ var character = "O";
 
 //handles x and o turns
 function turn(location){
-    
+   if (document.getElementById(location).innerHTML == "") {
     
        if(character == "X") {
        	  character = "O";
@@ -50,8 +50,9 @@ function turn(location){
         }
 
 	document.getElementById(location).innerHTML= character; 
+    }
+    checkwinner();
 }
-
 
 
 
@@ -68,3 +69,39 @@ document.getElementById("r3c1").innerHTML = "";
 document.getElementById("r3c2").innerHTML = "";
 document.getElementById("r3c3").innerHTML = "";
 }
+
+function checkwinner(){
+var r1c1 = document.getElementById("r1c1").innerHTML;
+var r1c2 = document.getElementById("r1c2").innerHTML;
+var r1c3 = document.getElementById("r1c3").innerHTML;
+var r2c1 = document.getElementById("r2c1").innerHTML;
+var r2c2 = document.getElementById("r2c2").innerHTML;
+var r2c3 = document.getElementById("r2c3").innerHTML;
+var r3c1 = document.getElementById("r3c1").innerHTML;
+var r3c2 = document.getElementById("r3c2").innerHTML;
+var r3c3 = document.getElementById("r3c3").innerHTML;
+
+	if(r1c1 == r1c2 && r1c1 == r1c3 && r1c1 != ""||
+	    r2c1 == r2c2 && r2c1 == r2c3 && r2c1 != ""||
+	    r3c1 == r3c2 && r3c1 == r3c3 && r3c1 != ""||
+	    r1c1 == r2c1 && r1c1 == r3c1 && r1c1 != ""||
+	    r1c2 == r2c2 && r1c2 == r3c2 && r1c2 != ""||
+	    r1c3 == r2c3 && r1c3 == r3c3 && r1c3 != ""||
+	    r3c1 == r2c2 && r3c1 == r1c3 && r3c1 != ""||
+	    r1c1 == r2c2 && r1c1 == r3c3 && r1c1 != ""){
+
+	alert("winner!");	
+	}
+
+
+
+
+}
+
+
+
+
+
+
+
+
