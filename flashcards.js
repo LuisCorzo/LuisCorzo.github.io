a22a633d839e8a4ecbebee6ab81232706861aea2
LuisCorzo <31773443+LuisCorzo@users.noreply.github.com>
2018-01-26 09:40:23 -0800

    :)

---
+++ flashcards.js
@@ -0,0 +1,10 @@
+function addNumbers(){
+
+var operand1 = document.getElementById("operand1").value;
+var operand2 = document.getElementById("operand2").value;
+var sum = operand1 + operand2;
+
+alert(sum);
+
+
+}
